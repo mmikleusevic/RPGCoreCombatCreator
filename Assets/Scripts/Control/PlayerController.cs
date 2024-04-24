808d258187127bcf6beeef9e5fcab623d1591daf
mikleusevicmarin <mikleusevicmarin@gmail.com>
2024-04-24 15:15:09 +0200

    Added basic enemy attacking

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,3 +1,4 @@
+using RPG.Combat;
 using RPG.Movement;
 using UnityEngine;
 
@@ -6,13 +7,39 @@ namespace RPG.Control
     public class PlayerController : MonoBehaviour
     {
         private Mover mover;
+        private Fighter fighter;
 
         private void Start()
         {
             mover = GetComponent<Mover>();
+            fighter = GetComponent<Fighter>();
         }
 
         private void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider != null)
+                {
+                    CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                    if (target == null) continue;
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        fighter.Attack(target);
+                    }
+                }
+            }          
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -22,15 +49,18 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit)
             {
                 mover.MoveTo(hit.point);
             }
+        }
+
+        private Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
