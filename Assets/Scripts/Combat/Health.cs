188c74d87377e56c65aa6b6688ac0770bec3bcf1
mikleusevicmarin <mikleusevicmarin@gmail.com>
2024-04-25 00:02:19 +0200

    Triggering death animation from code

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,12 +4,36 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
-        [SerializeField] private float health = 100f;
+        private const string DIE = "die";
+
+        [SerializeField] private float healthPoints = 100f;
+
+        private Animator animator;
+        private bool isDead = false;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
 
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(0, health - damage);
-            print(health);
+            healthPoints = Mathf.Max(0, healthPoints - damage);
+
+            if (healthPoints == 0 && !isDead)
+            {
+                Die();
+            }
+
+            print(healthPoints);
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+
+            isDead = true;
+            animator.SetTrigger(DIE);
         }
     }
 }
