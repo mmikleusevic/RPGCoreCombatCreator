29922a38689990492871c137fbfbce96b3d40ca0
mikleusevicmarin <mikleusevicmarin@gmail.com>
2024-04-24 19:22:46 +0200

    Changed prefabs so that player and enemy inherit from character

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -46,7 +46,7 @@ namespace RPG.Control
 
             if (hasHit)
             {
-                if (Input.GetMouseButtonDown(0))
+                if (Input.GetMouseButton(0))
                 {
                     mover.StartMoveAction(hit.point);
                 }
