e4d0b4830c8ffbcc8cf3d8e6e81cd8415ef1b59b
mikleusevicmarin <mikleusevicmarin@gmail.com>
2024-05-22 17:31:49 +0200

    Added damage logic to projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,6 +6,7 @@ public class Projectile : MonoBehaviour
     [SerializeField] private float speed = 1f;
 
     private Health target = null;
+    private float damage = 0;
 
     private void Update()
     {
@@ -20,9 +21,10 @@ public class Projectile : MonoBehaviour
         }
     }
 
-    public void SetTarget(Health target)
+    public void SetTarget(Health target, float damage)
     {
         this.target = target;
+        this.damage = damage;
     }
 
     private Vector3 GetAimLocation()
@@ -35,5 +37,13 @@ public class Projectile : MonoBehaviour
         }
 
         return target.transform.position + Vector3.up * targetCapsule.height / 2;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>() != target) return;
+
+        target.TakeDamage(damage);
+        Destroy(gameObject);
     }
 }
