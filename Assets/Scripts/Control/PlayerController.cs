d4fcb4392abc6ab26ff9af68efceeed3e2d7866c
mikleusevicmarin <mikleusevicmarin@gmail.com>
2024-06-04 11:30:01 +0200

    Added pixel cursors to player cursor

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,6 +1,7 @@
 using RPG.Attributes;
 using RPG.Combat;
 using RPG.Movement;
+using System;
 using UnityEngine;
 
 namespace RPG.Control
@@ -10,6 +11,23 @@ namespace RPG.Control
         private Mover mover;
         private Fighter fighter;
         private Health health;
+
+        private enum CursorType
+        {
+            None,
+            Movement,
+            Combat
+        }
+
+        [Serializable]
+        private struct CursorMapping
+        {
+            public CursorType cursorType;
+            public Texture2D texture;
+            public Vector2 hotspot;
+        }
+
+        [SerializeField] private CursorMapping[] cursorMappings = null;
 
         private void Start()
         {
@@ -24,6 +42,8 @@ namespace RPG.Control
 
             if (InteractWithCombat()) return;
             if (InteractWithMovement()) return;
+
+            SetCursor(CursorType.None);
         }
 
         private bool InteractWithCombat()
@@ -40,10 +60,29 @@ namespace RPG.Control
                 {
                     fighter.Attack(target.gameObject);
                 }
+
+                SetCursor(CursorType.Combat);
+
                 return true;
             }
 
             return false;
+        }
+
+        private void SetCursor(CursorType cursorType)
+        {
+            CursorMapping cursorMapping = GetCursorMapping(cursorType);
+            Cursor.SetCursor(cursorMapping.texture, cursorMapping.hotspot, CursorMode.Auto);
+        }
+
+        private CursorMapping GetCursorMapping(CursorType cursorType)
+        {
+            foreach (CursorMapping cursorMapping in cursorMappings)
+            {
+                if (cursorMapping.cursorType == cursorType) return cursorMapping;
+            }
+
+            return cursorMappings[0];
         }
 
         private bool InteractWithMovement()
@@ -57,9 +96,11 @@ namespace RPG.Control
                 {
                     mover.StartMoveAction(hit.point, 1f);
                 }
+
+                SetCursor(CursorType.Movement);
+
                 return true;
             }
-
             return false;
         }
 
