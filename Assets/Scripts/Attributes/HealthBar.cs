432741cda6e3effa01e283eca2e735cab4f61b96
mikleusevicmarin <mikleusevicmarin@gmail.com>
2024-06-06 15:57:24 +0200

    Disabled empty or full healthBars when not attacked

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -6,10 +6,18 @@ namespace RPG.Attributes
     {
         [SerializeField] private Health health;
         [SerializeField] private RectTransform foreground;
+        [SerializeField] private Canvas rootCanvas;
+
         private void Update()
         {
             float healthNormalized = health.GetFraction();
 
+            if (Mathf.Approximately(healthNormalized, 1) || Mathf.Approximately(healthNormalized,0))
+            {
+                rootCanvas.enabled = false;
+            }
+
+            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthNormalized, 1, 1);
         }
     }
